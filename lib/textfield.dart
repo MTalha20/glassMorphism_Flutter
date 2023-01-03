2161b1efdd636c8add4d2816d8fa4efb5c087ae9
MTalha20 <talha.wm15@gmail.com>
2023-01-03 13:21:58 +0500

    WorkComplete

--- lib/textfield.dart
+++ lib/textfield.dart
@@ -1,27 +1,29 @@
 import 'package:flutter/material.dart';
 
+Color fieldColor = Color.fromARGB(255, 13, 71, 161);
+
 Widget buildTextField(hinttext, controller, prefixicon){
   return Padding(
     padding: const EdgeInsets.all(12.0),
     child: TextField(
       style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
       controller: controller,
-      cursorColor:  Color.fromARGB(255, 13, 71, 161),
+      cursorColor:  fieldColor,
       decoration: InputDecoration(
         hintText: hinttext,
         fillColor: Colors.white,
         prefixIcon: prefixicon,
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(20)),
-          borderSide: BorderSide(color: Color.fromARGB(255, 13, 71, 161)),
+          borderSide: BorderSide(color: fieldColor),
         ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(20)),
-          borderSide: BorderSide(color: Color.fromARGB(255, 13, 71, 161)),
+          borderSide: BorderSide(color: fieldColor),
         ),
         disabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(20)),
-          borderSide: BorderSide(color: Color.fromARGB(255, 13, 71, 161),
+          borderSide: BorderSide(color: fieldColor,
         ),
       ),
     ),
